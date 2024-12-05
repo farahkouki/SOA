d939db7f52b5bc92134f437749966789834a8a4b
farahkouki <100531656+farahkouki@users.noreply.github.com>
2024-12-05 22:24:45 +0100

    Add files via upload

---
+++ soa1/src/main/java/org/example/outils/RestActivator.java
@@ -0,0 +1,6 @@
+package org.example.outils;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+@ApplicationPath("/api")
+public class RestActivator extends Application{
+}
---
+++ soa1/src/main/java/org/example/ressources/LogementRessources.java
@@ -0,0 +1,26 @@
+package org.example.ressources;
+
+import org.example.entities.Logement;
+import org.example.metiers.LogementBusiness;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+@Path("/logements")
+public class LogementRessources {
+    public static LogementBusiness logb = new LogementBusiness();
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response ajouterLogement(Logement l) {
+        if(logb.addLogement(l))
+            return Response.status(Status.CREATED).build();
+        else
+            return Response.status(Status.NOT_FOUND).build();
+    }
+}
